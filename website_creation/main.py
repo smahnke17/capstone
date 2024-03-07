53687da736be8adb13254a57de5942c000ef2ae8
smahnke17 <smahnke@umich.edu>
2024-03-06 21:18:51 -0500

    update to website

--- website_creation/main.py
+++ website_creation/main.py
@@ -1,10 +1,21 @@
 import streamlit as st
+from st_pages import Page, show_pages, add_page_title
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title='Competitive Breaking', layout='wide',
-                #    initial_sidebar_state=st.session_state.get('sidebar_state', 'collapsed'),
+#add_page_title()
+
+show_pages(
+    [
+        Page("./main.py", "Home", "🏠"),
+        Page("./pages/01_Player_Profiles.py", "Dancer Profiles", ":trophy:"),
+        Page("./pages/02_Judge_Profiles.py", "Judge Profiles", ":male-judge:"),
+        Page("./pages/03_Our_Data.py", "Our Data", ":male-technologist:"),
+    ]
 )
+#st.set_page_config(page_title='Competitive Breaking', layout='wide',
+                #    initial_sidebar_state=st.session_state.get('sidebar_state', 'collapsed'),
+#)
 
 st.markdown('Who Are We?')
 st.sidebar.markdown("Who Are We?")
